4db0d46ff0b47303ac25392977f6c3bd67d0cbd8
G24-EQ01\tarde <salinas.alvaro@protonmail.com>
2024-01-30 19:05:35 +0100

    Añadidas paginas y menu de navegacion

--- pages/urls.py
+++ pages/urls.py
@@ -3,4 +3,6 @@ from pages import views
 
 urlpatterns = [
     path("", views.home, name="home"),
+    path("projects", views.projects, name="projects"),
+    path("experience", views.experience, name="experience"),
 ]
--- pages/views.py
+++ pages/views.py
@@ -3,3 +3,9 @@ from django.shortcuts import render
 # Create your views here.
 def home(request):
     return render(request, "pages/home.html", {})
+
+def projects(request):
+    return render(request, "pages/projects.html", {})
+
+def experience(request):
+    return render(request, "pages/experience.html", {})
