595ba91938eb25892e64fde6af21bdd2d3e49ed8
noave <noavelasco@gmail.com>
2024-01-30 19:09:24 +0100

    Algunos cambios db

--- pages/models.py
+++ pages/models.py
@@ -1,3 +1,14 @@
 from django.db import models
 
 # Create your models here.
+class Project(models.Model):
+    name = models.CharField(max_length=100)
+    bio = models.TextField()
+    exp1 = models.TextField()
+    exp2 = models.TextField()
+    exp3 = models.TextField()
+    c_web = models.CharField(max_length=30)
+    c_red1 = models.CharField(max_length=30)
+    c_red2 = models.CharField(max_length=30)
+    c_email = models.CharField(max_length=30)
+    image = models.FileField(upload_to="users_images/", blank=True)
--- projects/models.py
+++ projects/models.py
@@ -5,5 +5,6 @@ from django.db import models
 class Project(models.Model):
     title = models.CharField(max_length=100)
     description = models.TextField()
-    technology = models.CharField(max_length=20)
+    tags = models.CharField(max_length=20)
     image = models.FileField(upload_to="project_images/", blank=True)
+    id_user = models.IntegerField(null=True)
