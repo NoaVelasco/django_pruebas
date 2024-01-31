be1050948fb4f72ccdc650148768585f3497b9c8
noave <noavelasco@gmail.com>
2024-01-31 20:59:10 +0100

    bases de datos hechas

--- projects/admin.py
+++ projects/admin.py
@@ -1,10 +1,17 @@
 from django.contrib import admin
-from projects.models import Project
+from projects.models import Experiences, Persona, Project
 
 
 # Register your models here.
+class ExperienceAdmin(admin.ModelAdmin):
+    pass
+
+class PersonaAdmin(admin.ModelAdmin):
+    pass
 class ProjectAdmin(admin.ModelAdmin):
     pass
 
 
+admin.site.register(Experiences, ExperienceAdmin)
+admin.site.register(Persona, PersonaAdmin)
 admin.site.register(Project, ProjectAdmin)
--- projects/models.py
+++ projects/models.py
@@ -5,7 +5,7 @@ from django.db import models
 class Project(models.Model):
     title = models.CharField(max_length=100)
     description = models.TextField()
-    tags = models.CharField(max_length=20)
+    tags = models.CharField(max_length=100)
     image = models.FileField(upload_to="project_images/", blank=True)
     # id_user = models.IntegerField(null=True)
     persona = models.ForeignKey(
@@ -17,11 +17,17 @@ class Project(models.Model):
 class Persona(models.Model):
     name = models.CharField(max_length=100)
     bio = models.TextField()
-    exp1 = models.TextField()
-    exp2 = models.TextField()
-    exp3 = models.TextField()
     c_web = models.CharField(max_length=30)
     c_red1 = models.CharField(max_length=30)
     c_red2 = models.CharField(max_length=30)
-    c_email = models.CharField(max_length=30)
+    email = models.CharField(max_length=30)
+    password = models.CharField(max_length=30, default="1234")
     image = models.FileField(upload_to="users_images/", blank=True)
+
+class Experiences(models.Model):
+    experience = models.TextField()
+    persona = models.ForeignKey(
+        "Persona",
+        on_delete=models.CASCADE,
+        null=True,
+    )
