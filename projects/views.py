9b8076ff1b448bf1f9e173127e60ca770e5d7b29
G24-EQ01\tarde <salinas.alvaro@protonmail.com>
2024-01-31 20:54:44 +0100

    Formulario para proyectos creado, todavía no interactua con la base de datos

--- projects/views.py
+++ projects/views.py
@@ -1,6 +1,6 @@
 from django.shortcuts import render
 from projects.models import Project
-
+from .forms import ProjectForm
 
 # Create your views here.
 def project_index(request):
@@ -13,3 +13,7 @@ def project_detail(request, pk):
     project = Project.objects.get(pk=pk)
     context = {"project": project}
     return render(request, "projects/project_detail.html", context)
+
+def project_new(request):
+    form=ProjectForm()
+    return render(request, 'projects/project_edit.html', {'form':form})
